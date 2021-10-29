1b7e88816e974edb0e362110fc508d2758a3ba8f
ARTEKASTO <86351729+ARTEKASTO@users.noreply.github.com>
2021-10-29 18:42:33 +0300

    0x95e0413831562846ae8eef0e75efac8faf05e3dc

---
+++ blok zincirleri/akıllı zincir/varlıklar/info.js
@@ -0,0 +1,11 @@
+    "name": "Artekasto Cash",
+    "website": "https://artekasto.com/",
+    "description": "We connect fans, players and clubs around the globe to provide a seamless virtual integration of all their top leagues, cups and championships.",
+    "explorer": "https://bscscan.com/token/0x95e0413831562846ae8eef0e75efac8faf05e3dc",
+    "type": "BEP20",
+    "symbol": "ATC",
+    "decimals": 18,
+    "status": "active",
+    "id": "0x95e0413831562846ae8eef0e75efac8faf05e3dc"
+}
+© 2021 GitHub, Inc.
